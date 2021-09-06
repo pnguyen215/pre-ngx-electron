2b8f4c7ab15536672b12c4187ff9c632a61e3961
Nguyen Huu Phuoc <nguyenhuuphuocit97@gmail.com>
2021-09-06 11:26:18 +0700

    :ok_hand: chore: add new dependencies #3

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+
 let win;
 
 function createWindow() {
